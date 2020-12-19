17f4dd08a956a7495d53c8dea1992083ac523ca2
Thomas Glauser <thomas@glauser.dev>
2020-12-19 16:57:19 +0100

    Repository init

---
+++ README.md
@@ -0,0 +1,97 @@
+# Twitch Livestream Data Scraper
+
+![Twitch scraper workflow](https://github.com/AUThomasCH/Toni-Scraper/workflows/Twitch%20scraper%20workflow/badge.svg)
+
+## Overview
+
+This application periodically saves certain livestream data through a Github Action Workflow through the Twitch API.
+The data is then stored in a Google Cloud SQL instance.
+
+## Dependencies
+
+- [A Google Cloud SQL Instance](https://cloud.google.com/sql)
+- Python 3.8
+- [python requests](https://pypi.org/project/requests/)
+- [python-dotenv](https://pypi.org/project/python-dotenv/)
+- [mysql-connector-python](https://dev.mysql.com/doc/connector-python/en/)
+
+## Setup
+
+### Get a Twitch API Token
+
+1. Register a Twitch developer application: https://dev.twitch.tv/console/apps
+2. Generate a new client secret.
+3. Now you have a client_id and a client_secret. Both values ​​must be saved in a Github secret. The GitHub action workflow will use those values for API authentication. The secrets must be named as follows:
+   - TWITCH_CLIENT_ID
+   - TWITCH_CLIENT_SECRET
+
+### Setup a Google Cloud SQL instance
+
+1. Setup a Google Cloud SQL instance.
+2. Create a new user with password authentication.
+3. Store the MySQL user credentials in GitHub Secrets as follows:
+   - MYSQL_USER
+   - MYSQL_PW
+4. Setup the Cloud SQL-Proxy and generate a private key. https://cloud.google.com/sql/docs/mysql/connect-external-app#proxy
+5. Store the value of the private key in the GitHub Secret GCP_SQL_KEY
+6. Store the value of the MySQL connection name in the GitHub Secret GCP_SQL_INSTANCE
+
+### Configure the Scraper
+
+1. Define the workflow schedule according to the crontab format. See https://crontab.guru/ for help.
+2. Define the Twitch user_id to scrape in the env variable TWITCH_USER_ID in the scraper.yml file.
+3. Define the MySQL Database name where the data is stored in the env variable MYSQL_DB in the scraper.yml file.
+
+## DB scheme:
+
+```sh
+TABLES['stream_data'] = (
+    "CREATE TABLE `stream_data` ("
+    "  `STREAM_LIVE` bool,"
+    "  `USER_ID` VARCHAR(30),"
+    "  `STREAM_ID` VARCHAR(30),"
+    "  `STREAM_TITLE` VARCHAR(100),"
+    "  `GAME_ID` VARCHAR(30),"
+    "  `GAME_NAME` VARCHAR(30),"
+    "  `START_TIMESTAMP` datetime,"
+    "  `CHECK_TIMESTAMP` datetime"
+    ") ENGINE=InnoDB")
+```
+
+## Table:
+
+```sh
+mysql> SELECT * FROM stream_data;
+
++-------------+-----------+-------------+---------------------------------------------------+---------+----------------+---------------------+---------------------+
+| STREAM_LIVE | USER_ID   | STREAM_ID   | STREAM_TITLE                                      | GAME_ID | GAME_NAME      | START_TIMESTAMP     | CHECK_TIMESTAMP     |
++-------------+-----------+-------------+---------------------------------------------------+---------+----------------+---------------------+---------------------+
+|           1 | XXXXXXXXX | XXXXXXXXXXX | XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX     | 65876   | Cyberpunk 2077 | 2020-12-19 10:03:59 | 2020-12-19 12:25:26 |
+|           1 | XXXXXXXXX | XXXXXXXXXXX | XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX     | 65876   | Cyberpunk 2077 | 2020-12-19 10:03:59 | 2020-12-19 12:31:40 |
+|           1 | XXXXXXXXX | XXXXXXXXXXX | XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX     | 65876   | Cyberpunk 2077 | 2020-12-19 10:03:59 | 2020-12-19 12:32:00 |
+|           1 | XXXXXXXXX | XXXXXXXXXXX | XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX     | 65876   | Cyberpunk 2077 | 2020-12-19 10:03:59 | 2020-12-19 12:32:43 |
+|           1 | XXXXXXXXX | XXXXXXXXXXX | XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX     | 65876   | Cyberpunk 2077 | 2020-12-19 10:03:59 | 2020-12-19 13:04:21 |
++-------------+-----------+-------------+---------------------------------------------------+---------+----------------+---------------------+---------------------+
+```
+
+| Database value  |                Description                |
+| --------------- | :---------------------------------------: |
+| STREAM_LIVE     |       True if the streamer is live        |
+| USER_ID         |           The streamers user_id           |
+| STREAM_ID       |     The ID of the current livestream      |
+| STREAM_TITLE    |         The current stream title          |
+| GAME_ID         |        The ID of the current game         |
+| GAME_NAME       |       The name of the current game        |
+| START_TIMESTAMP | The timestamp when the livestream started |
+| CHECK_TIMESTAMP |   The timestamp of the data scraper run   |
+
+**All timestamps are in UTC time!**
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com) before submitting pull requests.
+
+## License
+
+This is open source software
+[licensed as GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.en.html).
---
+++ scraper.py
@@ -0,0 +1,208 @@
+# Import modules
+# ==================================
+import datetime
+import json
+import os
+import sys
+from pathlib import Path
+
+import mysql.connector
+import requests
+from dotenv import load_dotenv
+from mysql.connector import errorcode
+
+# ==================================
+
+# Load .env
+# ==================================
+load_dotenv()
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+# ==================================
+
+# Read .env variables
+# ==================================
+TWITCH_CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
+TWITCH_CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
+TWITCH_USER_ID = os.getenv("TWITCH_USER_ID")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PW = os.getenv("MYSQL_PW")
+MYSQL_DB = os.getenv("MYSQL_DB")
+# ==================================
+
+# DB config
+# ==================================
+cnx = mysql.connector.connect(
+  host='127.0.0.1',
+  user=MYSQL_USER,
+  password=MYSQL_PW
+)
+# ==================================
+
+# Stream data
+# ==================================
+STREAM_LIVE = ''
+USER_ID = ''
+STREAM_ID = ''
+STREAM_TITLE = ''
+GAME_ID = ''
+GAME_NAME = ''
+START_TIMESTAMP = ''
+# ==================================
+
+# Get system time
+# ==================================
+CHECK_TIMESTAMP = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# ==================================
+
+# Get the auth token from twitch
+# ==================================
+def authenticate(client_id, client_secret):
+  try:
+    global TWITCH_AUTH_TOKEN
+    payload = {'client_id': client_id, 'client_secret': client_secret, 'grant_type': 'client_credentials', 'scope': 'analytics:read:extensions'}
+    r = requests.post('https://id.twitch.tv/oauth2/token', params=payload)
+
+    return json.loads(r.text)["access_token"]
+
+  except Exception as e:
+    print(sys.stderr, "Exception: %s" % str(e))
+    sys.exit(1)
+# ==================================
+
+# Get stream data
+# ==================================
+def get_stream_data(client_id, auth_token, user_id):
+  try:
+    global STREAM_LIVE
+    global USER_ID
+    global STREAM_ID
+    global STREAM_TITLE
+    global GAME_ID
+    global GAME_NAME
+    global START_TIMESTAMP
+
+    payload = {'user_id': user_id}
+    headers = {'client-id': client_id, 'Authorization': 'Bearer ' +  auth_token}
+    r = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=payload)
+
+    response = json.loads(r.text)["data"][0]
+
+    STREAM_LIVE = True
+    USER_ID = response["user_id"]
+    STREAM_ID = response["id"]
+    STREAM_TITLE = response["title"]
+    GAME_ID = response["game_id"]
+    GAME_NAME = response["game_name"]
+    START_TIMESTAMP = datetime.datetime.strptime(response["started_at"], '%Y-%m-%dT%H:%M:%SZ')
+  except:
+    STREAM_LIVE = False
+# ==================================
+
+# Execute API calls
+# ==================================
+TWITCH_AUTH_TOKEN = authenticate(TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET)
+get_stream_data(TWITCH_CLIENT_ID, TWITCH_AUTH_TOKEN, TWITCH_USER_ID)
+# ==================================
+
+# Output data
+# ==================================
+print("Received data:")
+print("======================================")
+print('STREAM_LIVE:         ', STREAM_LIVE)
+print('USER_ID:             ', USER_ID)
+print('STREAM_ID:           ', STREAM_ID)
+print('STREAM_TITLE:        ', STREAM_TITLE)
+print('GAME_ID:             ', GAME_ID)
+print('GAME_NAME:           ', GAME_NAME)
+print('START_TIMESTAMP:     ', START_TIMESTAMP)
+print('CHECK_TIMESTAMP:     ', CHECK_TIMESTAMP)
+print("======================================")
+print()
+# ==================================
+
+# Database handling
+# ==================================
+print("Database handling")
+print("======================================")
+
+# DB scheme
+# ==================================
+TABLES = {}
+TABLES['stream_data'] = (
+    "CREATE TABLE `stream_data` ("
+    "  `STREAM_LIVE` bool,"
+    "  `USER_ID` VARCHAR(30),"
+    "  `STREAM_ID` VARCHAR(30),"
+    "  `STREAM_TITLE` VARCHAR(100),"
+    "  `GAME_ID` VARCHAR(30),"
+    "  `GAME_NAME` VARCHAR(30),"
+    "  `START_TIMESTAMP` datetime,"
+    "  `CHECK_TIMESTAMP` datetime"
+    ") ENGINE=InnoDB")
+# ==================================
+
+# Create DB table function
+# ==================================
+def create_database(cursor):
+  try:
+    cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8mb4'".format(MYSQL_DB))
+  except mysql.connector.Error as err:
+    print("Failed creating DB: {}".format(err))
+    sys.exit(1)
+# ==================================
+
+cursor = cnx.cursor()
+
+# Check if DB exists
+# ==================================
+try:
+  cursor.execute("USE {}".format(MYSQL_DB))
+except mysql.connector.Error as err:
+  print("DB {} does not exist yet!".format(MYSQL_DB))
+  if err.errno == errorcode.ER_BAD_DB_ERROR:
+    create_database(cursor)
+    print("DB {} created successfully.".format(MYSQL_DB))
+    cnx.database = MYSQL_DB
+  else:
+    print("DB error: {}".format(err))
+    sys.exit(1)
+# ==================================
+
+# Check if table exists
+# ==================================
+for table_name in TABLES:
+  table_description = TABLES[table_name]
+  try:
+    print("Creating table {}: ".format(table_name), end='')
+    cursor.execute(table_description)
+  except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+      print("already exists.")
+    else:
+      print("DB error: {}".format(err))
+      sys.exit(1)
+  else:
+    print("OK")
+# ==================================
+
+# Data insert
+# ==================================
+add_stream_data = ("INSERT INTO stream_data "
+               "(STREAM_LIVE, USER_ID, STREAM_ID, STREAM_TITLE, GAME_ID, GAME_NAME, START_TIMESTAMP, CHECK_TIMESTAMP) "
+               "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+
+stream_data = (STREAM_LIVE, USER_ID, STREAM_ID, STREAM_TITLE, GAME_ID, GAME_NAME, START_TIMESTAMP, CHECK_TIMESTAMP)
+
+try:
+  cursor.execute(add_stream_data, stream_data)
+  cnx.commit()
+  print("Data insertion successful.")
+except mysql.connector.Error as err:
+  print("Data insertion failed: {}".format(err))
+  sys.exit(1)
+
+cursor.close()
+cnx.close()
+print("======================================")
+# ==================================
